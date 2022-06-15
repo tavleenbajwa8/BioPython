1f4aaed47eb6e6c62750640ee2f3ad25ef892787
Tavleen Bajwa <86622630+tavleenbajwa8@users.noreply.github.com>
2022-06-15 11:22:41 +0530

    Add files via upload

---
+++ Convert names in a list to proper noun.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Have list of names with all the items in small case and convert the items to proper noun
+
+# In[1]:
+
+
+l = ["tavleen", "aaleen", "ravleen", "navleen"]
+list(map(lambda a: a.title(), l))
+
+
+# l = [1, 2, 3, 4] to l = [1, 4, 9, 16]
+
+# In[2]:
+
+
+l = [1, 2, 3, 4]
+list(map(lambda i: i*i, l))
+
+
+# In[ ]:
+
+
+
+
